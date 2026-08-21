efb2d32d179ca605aa4338c1b31ba457a696e79e
SchoolHacksRUs <112196142+SchoolHacksRUs@users.noreply.github.com>
2026-08-21 13:34:18 -0400

    Update games.js

--- games.js
+++ games.js
@@ -1,4 +1,5 @@
 const games = [
       { title: "PEAK", desc: "A physics-based climbing game where you scale a procedurally generated mountain with realistic movement.", url: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/games/471.html", image: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/images/471.jpg" },
       { title: "Trombone Champ", desc: "A rhythm game where you play the trombone by sliding your mouse to match musical notes.", url: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/games/472.html", image: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/images/472.jpg" },
+            { title: "Trombone Champ", desc: "A rhythm game where you play the trombone by sliding your mouse to match musical notes.", url: "https://raw.githubusercontent.com/SchoolHacksRUs/fusion-files-games/refs/heads/main/stardew/stardew.html", image: "https://raw.githubusercontent.com/NoahsAmazingTutoringHelp/Noahs-Calculus-Tutor/refs/heads/master/images/444.jpg" },
 ];
